17f76b94d156e94da76a0ef06ced986594ace0b1
Clepson Lacerda <clepsonsantos9@gmail.com>
2024-02-20 11:31:38 -0300

    feat: add a loading page to home page

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,3 +1,12 @@
+import { Skaleton } from '@/components/skeleton'
+
 export default function HomeLoading() {
-  return <p>Carregando...</p>
+  return (
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skaleton className="col-span-6 row-span-6 h-[856px]" />
+
+      <Skaleton className="col-span-3 row-span-3" />
+      <Skaleton className="col-span-3 row-span-3" />
+    </div>
+  )
 }
